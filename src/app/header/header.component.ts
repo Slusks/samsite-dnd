7cfffa374e30640106680e6f9d27ac4d9b442cd5
Slusks <samslusky@gmail.com>
2020-04-17 06:34:33 -0400

    Addition of Logout Button

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import { AuthGuard} from '../AuthenticationPackage/core/auth.guard';
+import { AuthService } from '../AuthenticationPackage/core/auth.service';
+import { Router, Params } from '@angular/router';
+//import { UserService } from './user.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, AfterViewInit{
 
-  constructor() {}
+  loading;
+ 
+
+  constructor(private auth: AuthGuard,
+              private authService: AuthService,
+              private router: Router,) {}
+
+  ngOnInit(){
+    this.loading = true;
+    
+
+    
+  }
+
+  tryLogout(){
+    this.authService.doLogout()
+    .then(res => {
+      this.router.navigate(['../login']);
+    })
+  }
+
+  ngAfterViewInit(){
+    this.loading = false;
+  }
 
 }
