3dee8f19efd3c39dcffd026f529db31b853ab3c4
Slusks <samslusky@gmail.com>
2020-05-17 05:59:22 -0400

    Faction 2 Update 2
    Spent a lot of time trying a lot of different things.

--- src/app/factions2/factions2.component.ts
+++ src/app/factions2/factions2.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { gsap, TweenMax, TweenLite } from 'gsap';
 import { Draggable } from 'gsap/Draggable';
 import { InertiaPlugin } from 'gsap/InertiaPlugin';
+import { AuthService } from 'src/app/AuthenticationPackage/core/auth.service'
 
 
 gsap.registerPlugin(Draggable);
@@ -18,50 +19,72 @@ export class Factions2Component implements OnInit {
 
 	cardArray$;
 
-	cardObject: Object = {
+	TCcardObject: Object = {
 		EmeraldEnclave:{
 			name: "The Emerald Enclave",
 			description: "Coming soon",
-			img: "assets/pics/emerald-enclave-crest.png"
+			img: "assets/pics/emerald-enclave-crest.png",
 		},
-		Harpers:{
+		harpers:{
 			name: "The Harpers",
 			description: "Coming soon",
-			img: "assets/pics/the-harpers-crest.png"
+			img: "assets/pics/the-harpers-crest.png",
 		},
 		lordsalliance:{
 			name: "The Lords Alliance",
 			description: "Coming soon",
-			img: "assets/pics/lords-alliance-crest.png"
+			img: "assets/pics/lords-alliance-crest.png",
+		},
+		orderofthegauntlet:{
+			name: "The Order of the Gauntlet",
+			description: "Coming soon",
+			img: "assets/pics/order-of-gauntlet-crest.png",
+		},
+		zhentarim:{
+			name: "The Zhentarim",
+			description: "Coming soon",
+			img: "assets/pics/zhentarim-crest.png",
 		},
 	}
 
+	MCcardObject: Object = {
+		dwendalianempire:{
+			name: "The Dwendalian Empire",
+			description: "Coming soon",
+			img: "assets/pics/dwendalian-empire-crest.png",
+		},
+		kryndynasty:{
+			name: "The Kryn Dynasty",
+			description: "Coming soon",
+			img: "assets/pics/kryn-dynasty-crest.png",
+		},
+		clovisconcord:{
+			name: "The Clovis Concord",
+			description: "Coming soon",
+			img: "assets/pics/clovis-concord-crest.png",
+		},
+		myriad:{
+			name: "The Myriad",
+			description: "Coming soon",
+			img: "assets/pics/the-myriad-crest.png",
+		},
+		revelry:{
+			name: "The Revelry",
+			description: "Coming soon",
+			img: "assets/pics/the-revelry-crest.png",
+		},
+	}
+
+
 	
 
 
 
 
 
-  constructor(  ) { }
+  constructor( public authService: AuthService ) { }
 
-  ngOnInit(){
+  ngOnInit(){  }
 
-	Draggable.create("#test-card", {
-		type: "x,y",
-		onThrowUpdate: this.throwFunction
-		})
-	
-
-
-
-  }
-
-  ngAfterViewInit(){
-	Draggable.create("#card", {type: "x,y"})
-  }
-
-  throwFunction(){}
-
-  }
-
+}
 
